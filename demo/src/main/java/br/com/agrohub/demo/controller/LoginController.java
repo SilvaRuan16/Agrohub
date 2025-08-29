4ff1a63d8cb80eca99e329115f1653c980a0b0cf
Pedro Henrique A. Herzog <pedrohaherzog2005@gmail.com>
2025-08-28 21:42:18 -0300

    refactor: Comentar importações e métodos não utilizados no LoginController.

--- demo/src/main/java/br/com/agrohub/demo/controller/LoginController.java
+++ demo/src/main/java/br/com/agrohub/demo/controller/LoginController.java
@@ -1,15 +1,19 @@
 package br.com.agrohub.demo.controller;
 
-import java.math.BigInteger;
-import java.util.List;
+// import java.math.BigInteger;
+// import java.util.List;
 
-import br.com.agrohub.demo.model.Login;
+// import br.com.agrohub.demo.models.LoginModel;
 import br.com.agrohub.demo.service.LoginService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.web.bind.annotation.DeleteMapping;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.PutMapping;
+// import org.springframework.web.bind.annotation.RequestBody;
+// import org.springframework.web.bind.annotation.PathVariable;
+// import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,30 +22,30 @@ public class LoginController {
     @Autowired
     LoginService LoginService;
 
-    @GetMapping("/login")
-    public List<Login> getTodos(@RequestParam String userAccess) throws Exception {
-        return LoginService.getTodos();
-    }
+    // @GetMapping("/login")
+    // public List<LoginModel> getTodos(@RequestParam String userAccess) throws Exception {
+    //     return LoginService.getTodos();
+    // }
 
-    @GetMapping("/login/{id}")
-    public Login buscarPorId(@PathVariable BigInteger id) throws Exception {
-        return LoginService.buscarPorId(id);
-    }
+    // @GetMapping("/login/{id}")
+    // public LoginModel buscarPorId(@PathVariable BigInteger id) throws Exception {
+    //     return LoginService.buscarPorId(id);
+    // }
 
-    @PostMapping("/login")
-    public login cadastrar(@RequestBody Login login) throws Exception {
-        return LoginService.cadastrar(login);
-    }
+    // @PostMapping("/login")
+    // public LoginModel cadastrar(@RequestBody LoginModel login) throws Exception {
+    //     return LoginService.cadastrar(login);
+    // }
 
-    @PutMapping("/login/{id}")
-    public Login atualizar(@PathVariable BigInteger id, @RequestBody Login login) throws Exception {
-        return LoginService.atualizar(id, login);
-    }
+    // @PutMapping("/login/{id}")
+    // public LoginModel atualizar(@PathVariable BigInteger id, @RequestBody LoginModel login) throws Exception {
+    //     return LoginService.atualizar(id, login);
+    // }
 
-    @DeleteMapping("/login/{id}")
-    public void deletar(@PathVariable BigInteger id) throws Exception {
-        LoginService.deletar(id);
-    }
+    // @DeleteMapping("/login/{id}")
+    // public void deletar(@PathVariable BigInteger id) throws Exception {
+    //     LoginService.deletar(id);
+    // }
 
     
 
