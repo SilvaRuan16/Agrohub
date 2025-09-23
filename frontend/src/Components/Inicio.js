c318da879c605fca7246b5eef789dce17178b469
Seu Nome <seu@email.com>
2025-09-23 09:22:07 -0300

    Estado tela de produtos

--- frontend/src/Components/Inicio.js
+++ frontend/src/Components/Inicio.js
@@ -1,13 +1,14 @@
 import MenuBar from "./Others/MenuBar";
 import Menuzinho from "./Others/Menuzinho";
+import '../Components/Style/Inicio.css';
 
 export default function InicioEmpresa() {
     return <>
         <MenuBar/>
         <div className="bg_bodyInicio">
             <Menuzinho/>
-            <div>
-
+            <div className="boasVindas">
+                <h2>Seja bem vindo!</h2>
             </div>
         </div>
     </>
